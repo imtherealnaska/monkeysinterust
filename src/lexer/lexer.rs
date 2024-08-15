e7a8706eeb28ed077a096858892461f494c1ed61
Narendra <naskastark11@gmail.com>
2024-08-15 18:27:59 +0530

    This thing works for 2nd test , always use dbg() for debugging .

--- src/lexer/lexer.rs
+++ src/lexer/lexer.rs
@@ -1,4 +1,4 @@
-use super::token::{self, Token, TokenType, Tokens};
+use super::token::{Token, Tokens};
 
 pub(crate) struct Lexer {
     input: String,
@@ -8,19 +8,32 @@ pub(crate) struct Lexer {
 }
 
 pub trait LexerTrait {
+    fn read_number(&mut self) -> String;
     fn new(input: &str) -> Self;
     fn read_char(&mut self);
     fn next_token(&mut self) -> Token;
+    fn read_identifier(&mut self) -> String;
+    fn skip_whitespace(&mut self);
 }
 
-// fn new_token(token_type: TokenType, ch: u8) -> Token {
-//     Token {
-//         type_: todo!(),
-//         literal: todo!(),
-//     }
-// }
+fn is_letter(ch: char) -> bool {
+    ch.is_alphanumeric() || ch == '_'
+}
+
+fn is_digit(ch: Option<char>) -> bool {
+    let dif = ch.unwrap();
+    dif.is_ascii_digit()
+}
 
 impl LexerTrait for Lexer {
+    fn read_number(&mut self) -> String {
+        let pos = self.position;
+        while is_digit(self.ch) {
+            self.read_char();
+        }
+        self.input[pos..self.position].to_string()
+    }
+
     fn new(input: &str) -> Self {
         let mut lexer = Self {
             input: input.to_string(),
@@ -38,25 +51,73 @@ impl LexerTrait for Lexer {
         } else {
             self.ch = self.input[self.read_position..].chars().next();
         }
+        dbg!(&self.input[self.position..]);
         self.position = self.read_position;
         self.read_position += 1;
     }
 
     fn next_token(&mut self) -> Token {
         let tok;
+        self.skip_whitespace();
+        println!("once");
         match self.ch {
             Some('=') => tok = Token::new(&Tokens::Assign.to_string(), "="),
-            Some(';') => tok = Token::new(&Tokens::Semicolon.to_string(), ";"),
+            Some(';') => {
+                print!("reading ;");
+                tok = Token::new(&Tokens::Semicolon.to_string(), ";")
+            }
             Some('(') => tok = Token::new(&Tokens::LParen.to_string(), "("),
             Some(')') => tok = Token::new(&Tokens::RParen.to_string(), ")"),
             Some(',') => tok = Token::new(&Tokens::Comma.to_string(), ","),
             Some('+') => tok = Token::new(&Tokens::Plus.to_string(), "+"),
             Some('{') => tok = Token::new(&Tokens::LBrace.to_string(), "{"),
             Some('}') => tok = Token::new(&Tokens::RBrace.to_string(), "}"),
+            Some(ch) if is_letter(ch) => {
+                let literal = self.read_identifier();
+                //can use lookup_ident() but this seems ok
+                tok = match literal.as_str() {
+                    "let" => Token::new(&Tokens::Let.to_string(), &literal),
+                    "fn" => Token::new(&Tokens::Function.to_string(), &literal),
+                    _ => Token::new(&Tokens::Ident(literal.clone()).to_string(), &literal),
+                };
+            }
+            Some(ch) if ch.is_digit(10) => {
+                let literal = self.read_number();
+                tok = Token::new(&Tokens::Int(literal.clone()).to_string(), &literal);
+            }
+
             None => tok = Token::new(&Tokens::Eof.to_string(), ""),
-            _ => tok = Token::new("ILLEGAL", &self.ch.unwrap().to_string()),
+            _ => {
+                tok = Token::new(
+                    &Tokens::Illegal.to_string(),
+                    &self.ch.unwrap_or('\0').to_string(),
+                )
+            }
         };
         self.read_char();
         tok
+    }
+
+    fn read_identifier(&mut self) -> String {
+        let pos = self.position;
+        while is_letter(self.ch.unwrap()) {
+            self.read_char();
+        }
+        String::from(&self.input[pos..self.position])
+    }
+
+    fn skip_whitespace(&mut self) {
+        while let Some(ch) = self.ch {
+            if self.ch == Some(' ')
+                || self.ch == Some('\t')
+                || self.ch == Some('\n')
+                || self.ch == Some('\r')
+            {
+                println!("skipping whitespace");
+                self.read_char();
+            } else {
+                break;
+            }
+        }
     }
 }
--- src/lexer/token.rs
+++ src/lexer/token.rs
@@ -1,8 +1,26 @@
+use std::collections::HashMap;
+
 pub type TokenType = String;
 
 pub struct Token {
-    type_: String,
-    literal: String,
+    pub type_: String,
+    pub literal: String,
+}
+
+pub fn keywords() -> HashMap<String, Tokens> {
+    let mut keywords = HashMap::new();
+    keywords.insert("fn".to_string(), Tokens::Function);
+    keywords.insert("let".to_string(), Tokens::Let);
+    keywords
+}
+
+pub fn lookup_ident(ident: &str) -> Tokens {
+    let kw = keywords();
+    if let Some(token_type) = kw.get(ident) {
+        token_type.clone()
+    } else {
+        Tokens::Ident(ident.to_string())
+    }
 }
 
 impl Token {
@@ -14,7 +32,7 @@ impl Token {
     }
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, PartialEq, Eq, Clone)]
 pub enum Tokens {
     Illegal,
     Eof,
@@ -66,16 +84,38 @@ mod tests {
 
     #[test]
     fn test_next_token() {
-        let input = "=+(){},;";
+        let input = "let five = 5 ;
+            let ten = 10 ;
+            let add = fn ( x , y ) {
+            x + y ;
+            } ;";
 
         let tests = vec![
+            (Tokens::Let, "let"),
+            (Tokens::Ident("five".to_string()), "five"),
             (Tokens::Assign, "="),
-            (Tokens::Plus, "+"),
+            (Tokens::Int("5".to_string()), "5"),
+            (Tokens::Semicolon, ";"),
+            (Tokens::Let, "let"),
+            (Tokens::Ident("ten".to_string()), "ten"),
+            (Tokens::Assign, "="),
+            (Tokens::Int("10".to_string()), "10"),
+            (Tokens::Semicolon, ";"),
+            (Tokens::Let, "let"),
+            (Tokens::Ident("add".to_string()), "add"),
+            (Tokens::Assign, "="),
+            (Tokens::Function, "fn"),
             (Tokens::LParen, "("),
+            (Tokens::Ident("x".to_string()), "x"),
+            (Tokens::Comma, ","),
+            (Tokens::Ident("y".to_string()), "y"),
             (Tokens::RParen, ")"),
             (Tokens::LBrace, "{"),
+            (Tokens::Ident("x".to_string()), "x"),
+            (Tokens::Plus, "+"),
+            (Tokens::Ident("y".to_string()), "y"),
+            (Tokens::Semicolon, ";"),
             (Tokens::RBrace, "}"),
-            (Tokens::Comma, ","),
             (Tokens::Semicolon, ";"),
             (Tokens::Eof, ""),
         ];
